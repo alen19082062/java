1bda7545f790921e2d354fa7bd79120c8057d9fe
fengjy <19082062@qq.com>
2020-03-01 16:34:23 +0800

    add test.txt

--- examples/concurrent/src/com/gg/concurrent/juc/CyclicBarrierTest.java
+++ examples/concurrent/src/com/gg/concurrent/juc/CyclicBarrierTest.java
@@ -13,18 +13,21 @@ public class CyclicBarrierTest {
     public static void main(String[] args) {
 
         System.out.println(ThreadUtil.toNameString() + "  main() start ... " );
+        System.out.println(ThreadUtil.toNameString() + "  main() CyclicBarrier size : " + SIZE );
 
-        // 参数parties指让多少个线程或者任务等待至barrier状态；
+        // 参数parties指让多少个线程或者任务等待至barrier状态；这里是 5 个
         // 参数barrierAction为当这些线程都达到barrier状态时会执行的内容。
         cb = new CyclicBarrier(SIZE, new Runnable() {
             public void run() {
-                System.out.println(ThreadUtil.toNameString() + " 等待 5s ") ;
+                System.out.println(ThreadUtil.toNameString() + " 到达 barrier 状态，执行 barrierAction ");
+                System.out.println(ThreadUtil.toNameString() + " CyclicBarrier's parties is: " + cb.getParties());
+                System.out.println(ThreadUtil.toNameString() + " barrierAction work for 5s ") ;
                 try {
                     Thread.sleep(5000);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
-                System.out.println(ThreadUtil.toNameString() + " 到达 barrier 状态，CyclicBarrier's parties is: " + cb.getParties());
+                System.out.println(ThreadUtil.toNameString() + " barrierAction work end ... ") ;
             }
         });
 
@@ -37,9 +40,11 @@ public class CyclicBarrierTest {
         public void run() {
             try {
                 System.out.println(ThreadUtil.toNameString() + " wait for CyclicBarrier.");
+                System.out.println(ThreadUtil.toNameString() + " before cb.await() cb = " + cb.getNumberWaiting() );
 
                 // 将cb的参与者数量加1
                 cb.await();
+                System.out.println(ThreadUtil.toNameString() + " after cb.await()  cb = " + cb.getNumberWaiting() );
 
                 // cb的参与者数量等于5时，才继续往后执行
                 System.out.println(ThreadUtil.toNameString() + " continued.");
